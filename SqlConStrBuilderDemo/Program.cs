dc415d839a0e7f9d92b9f037e383997f30645a79
CarsonMax <luzhucaine@outlook.com>
2020-09-25 17:15:51 +0800

    update

--- SqlConStrBuilderDemo/Program.cs
+++ SqlConStrBuilderDemo/Program.cs
@@ -7,7 +7,8 @@ namespace SqlConStrBuilderDemo
     class Program
     {
         //const string connStr = @"Data Source=.;Initial Catalog=EFTestDb;User ID=sa;Password=sa1234=\/=/=\";
-        const string connStr = @"Data Source=.;Initial Catalog=EFTestDb;User ID=secuser;Password=sa1234=\/=/=\";
+        const string connStr = @"Initial Catalog=EFTestDb;User ID=secuser;Password=sa;1234;=\/=/=\;Data Source=.;";
+        const string connStrConverted = "Data Source=.;Initial Catalog=EFTestDb;User ID=secuser;Password=\"sa;1234;=\\/=/=\\\"";
         static void Main(string[] args)
         {
             //Console.WriteLine(Convert());
@@ -25,11 +26,23 @@ namespace SqlConStrBuilderDemo
             return sqlConStrBuilder.Password;
         }
 
+        static string CovertFromProperties()
+        {
+            var sqlConStrBuilder = new SqlConnectionStringBuilder();
+            sqlConStrBuilder.DataSource = ".";
+            sqlConStrBuilder.InitialCatalog = "EFTestDb";
+            sqlConStrBuilder.UserID = "secuser";
+            sqlConStrBuilder.Password = @"sa;1234;=\/=/=\";
+
+            return sqlConStrBuilder.ConnectionString;
+        }
+
         static void TestOpen()
         {
             DataSet ds = new DataSet();
             var conn = new SqlConnection();
-            conn.ConnectionString = connStr;
+            //conn.ConnectionString = connStr;
+            conn.ConnectionString = CovertFromProperties();
             //conn.Open();
             var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from EXT_GAP_INITIATIVE_ATTACHMENT";
