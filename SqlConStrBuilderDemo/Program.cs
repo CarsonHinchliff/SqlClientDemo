3a00203dd7246c7f9537ec03109766558f5d15b2
CarsonMax <luzhucaine@outlook.com>
2020-09-25 20:36:48 +0800

    updated

--- SqlConStrBuilderDemo/Program.cs
+++ SqlConStrBuilderDemo/Program.cs
@@ -7,12 +7,14 @@ namespace SqlConStrBuilderDemo
     class Program
     {
         //const string connStr = @"Data Source=.;Initial Catalog=EFTestDb;User ID=sa;Password=sa1234=\/=/=\";
+        const string connStrLowerCase = @"initial catalog=eftestdb;user id=secuser;password=sa;1234;=\/=/=\;data source=.;";
         const string connStr = @"Initial Catalog=EFTestDb;User ID=secuser;Password=sa;1234;=\/=/=\;Data Source=.;";
         const string connStrConverted = "Data Source=.;Initial Catalog=EFTestDb;User ID=secuser;Password=\"sa;1234;=\\/=/=\\\"";
         static void Main(string[] args)
         {
             //Console.WriteLine(Convert());
-            TestOpen();
+            //TestOpen();
+            Console.WriteLine(GetDbName(connStrConverted));
 
             Console.WriteLine("Test done");
             Console.ReadKey();
@@ -30,11 +32,16 @@ namespace SqlConStrBuilderDemo
         {
             var sqlConStrBuilder = new SqlConnectionStringBuilder();
             sqlConStrBuilder.DataSource = ".";
-            sqlConStrBuilder.InitialCatalog = "EFTestDb";
-            sqlConStrBuilder.UserID = "secuser";
             sqlConStrBuilder.Password = @"sa;1234;=\/=/=\";
+            sqlConStrBuilder.InitialCatalog = @"EF;=\/=/=\TestDb";
+            sqlConStrBuilder.UserID = "secuser";
 
             return sqlConStrBuilder.ConnectionString;
+        }
+
+        public static string GetDbName(string connString)
+        {
+            return new SqlConnectionStringBuilder(connString).InitialCatalog;
         }
 
         static void TestOpen()
